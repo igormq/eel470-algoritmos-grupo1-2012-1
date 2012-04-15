dd7a2410882b6f51f97cf6523c49c92730473332
igormq@poli.ufrj.br <igormq@poli.ufrj.br@78dd2318-ba90-212c-0590-2dc8b4d45739>
2012-04-15 03:25:21 +0000

    Começo do Trabalho de Algoritmo e Estruturas de Dados

---
+++ trunk/Statistics/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: igormq
+ *
+ * Created on April 15, 2012, 12:20 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
