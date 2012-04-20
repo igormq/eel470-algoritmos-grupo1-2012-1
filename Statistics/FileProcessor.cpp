b749ae9ce55c4b267430734600675345e2c62e2c
igormq@poli.ufrj.br <igormq@poli.ufrj.br@78dd2318-ba90-212c-0590-2dc8b4d45739>
2012-04-20 00:02:31 +0000

    FileProcessor new header file

--- Statistics/FileProcessor.cpp
+++ Statistics/FileProcessor.cpp
@@ -5,9 +5,13 @@
  * Created on 17 de Abril de 2012, 13:48
  */
 
+#include <istream>
+#include <bits/stl_pair.h>
+
 #include "FileProcessor.h"
 
-FileProcessor::FileProcessor(std::string path) {
+FileProcessor::FileProcessor() {
+//    this->stringStatistics = new StringStatistics();
 }
 
 FileProcessor::FileProcessor(const FileProcessor& orig) {
@@ -16,6 +20,10 @@ FileProcessor::FileProcessor(const FileProcessor& orig) {
 FileProcessor::~FileProcessor() {
 }
 
-void FileProcessor::addToStatistics(StringStatistics &){
+void FileProcessor::addFile(const string & filePath) {
+    this->fileQueue.push_back(filePath);
+}
+
+map<string,int> FileProcessor::getStatistics() const{
     
 }
