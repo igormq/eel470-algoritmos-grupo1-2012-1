90230ade83b228704c7538dee6be34e3f71c10f0
igormq@poli.ufrj.br <igormq@poli.ufrj.br@78dd2318-ba90-212c-0590-2dc8b4d45739>
2012-04-20 03:03:41 +0000

    String Statistics implementation

--- trunk/Statistics/StringStatistics.cpp
+++ trunk/Statistics/StringStatistics.cpp
@@ -7,7 +7,8 @@
 
 #include "StringStatistics.h"
 
-StringStatistics::StringStatistics() {
+StringStatistics::StringStatistics(){
+    
 }
 
 StringStatistics::StringStatistics(const StringStatistics& orig) {
@@ -17,12 +18,39 @@ StringStatistics::StringStatistics(const StringStatistics& orig) {
 StringStatistics::~StringStatistics() {
 }
 
-bool StringStatistics::add(std::string){
-    
-}
 
-bool StringStatistics::resetStatistics(){
+/**This method will verify if there is already a string on the statistic,
+ *increasing the counter. If there isn't, will insert a new object to 
+ *the statistic.
+ * @param inStr, the string that will iterate to the statistics
+ */
+void StringStatistics::add(std::string inStr){
+    bool found = false;
+    std::set<StringCounter>::iterator it;
     
+    for ( it=this->data.begin() ; it != this->data.end(); it++ ){
+        if(*it==inStr){
+            *it++;
+            found=true;
+        } 
+    }
+    
+    if(!found){
+        StringCounter temp (inStr);
+        this->data.insert(temp);        
+    }
+    
+       
+}
+/**
+ *All the elements in the set container are dropped, 
+ *@params 
+ *@return true if the set was really cleared
+*/
+bool StringStatistics::resetStatistics(){
+    this->data.clear();
+    //Checks if it's really empty.
+    return this->data.empty();
 }
 
 std::set<StringCounter> StringStatistics::getData() const{
